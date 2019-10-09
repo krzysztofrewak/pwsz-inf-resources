167e6748fbd86af4e4cef029ec68e71edb0e3f51
Krzysztof Rewak <krzysztof.rewak@itmation.pl>
2019-10-09 08:51:03 +0200

    syntax style

--- s3-inf-ppo/laboratoria/lab02/lab02a.php
+++ s3-inf-ppo/laboratoria/lab02/lab02a.php
@@ -2,19 +2,20 @@
 
 const STUDENTS_COUNT = 10;
 
-function getRandomStudentNumber(): string {
+function getRandomStudentNumber(): string
+{
 	$randomStudentNumber = rand() % 2000 + 38000;
-	return (string) $randomStudentNumber;
+	return (string)$randomStudentNumber;
 }
 
 $students = [];
 
-for($i = 0; $i < STUDENTS_COUNT; $i++) {
+for ($i = 0; $i < STUDENTS_COUNT; $i++) {
 	$students[] = getRandomStudentNumber();
 }
 
 echo "Students group have been filled." . PHP_EOL . PHP_EOL;
 
-for($i = 0; $i < sizeof($students); $i++) {
+for ($i = 0; $i < sizeof($students); $i++) {
 	echo $students[$i] . PHP_EOL;
 }
--- s3-inf-ppo/laboratoria/lab02/lab02b.php
+++ s3-inf-ppo/laboratoria/lab02/lab02b.php
@@ -2,36 +2,38 @@
 
 const STUDENTS_COUNT = 10;
 
-class Student {
+class Student
+{
+    public $studentNo;
 
-	public $studentNo;
+    public function setStudentNo(string $studentNo): void
+    {
+        $this->studentNo = $studentNo;
+    }
 
-	public function setStudentNo(string $studentNo): void {
-		$this->studentNo = $studentNo;
-	}
-		
-	public function getStudentNo(): string {
-		return $this->studentNo;
-	}
-
+    public function getStudentNo(): string
+    {
+        return $this->studentNo;
+    }
 }
 
-function getRandomStudentNumber(): string {
-	$randomStudentNumber = rand() % 2000 + 38000;
-	return (string) $randomStudentNumber;
+function getRandomStudentNumber(): string
+{
+    $randomStudentNumber = rand() % 2000 + 38000;
+    return (string)$randomStudentNumber;
 }
 
 $students = [];
 
-for($i = 0; $i < STUDENTS_COUNT; $i++) {
-	$student = new Student();
-	$student->setStudentNo(getRandomStudentNumber());
-	$students[] = $student;
+for ($i = 0; $i < STUDENTS_COUNT; $i++) {
+    $student = new Student();
+    $student->setStudentNo(getRandomStudentNumber());
+    $students[] = $student;
 }
 
 echo "Students group have been filled." . PHP_EOL . PHP_EOL;
 
-for($i = 0; $i < sizeof($students); $i++) {
-	$student = $students[$i];
-	echo $student->getStudentNo() . PHP_EOL;
+for ($i = 0; $i < sizeof($students); $i++) {
+    $student = $students[$i];
+    echo $student->getStudentNo() . PHP_EOL;
 }

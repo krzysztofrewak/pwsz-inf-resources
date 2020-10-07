c5c1db91c820b12bb22392bbc3ebef266bb5573e
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2020-10-07 13:30:06 +0200

    ppsi2/w01 rework

--- s5-pam-ppsi2/projekty/app/data/projects.js
+++ s5-pam-ppsi2/projekty/app/data/projects.js
@@ -1,134 +1,218 @@
 let sourceProjects = [
-	{
-		"active": false,
-		"title": "pizza-order-generator",
-		"features": [
-			"authentication (also social media)",
-			"administrator CRUD",
-			"multitenancy",
-			"drag&drop frontend",
-			"real-time actions",
-			"traffic analytics & logs"
-		]
-	},
-	{
-		"active": false,
-		"title": "familiada-online",
-		"features": [
-			"authentication (also social media)",
-			"administrator CRUD",
-			"front/backend separation",
-			"real-time multi-user game",
-			"games archive",
-			"traffic analytics"
-		]
-	},
-	{
-		"active": false,
-		"title": "ebook-self-publisher",
-		"features": [
-			"authentication",
-			"administrator CRUD",
-			"payment plans",
-			"advanced WYSIWYG editor",
-			"file conversion",
-			"logs"
-		]
-	},
-	{
-		"active": false,
-		"title": "wedding-planner",
-		"features": [
-			"authentication (also social media)",
-			"administrator CRUD",
-			"invitations and RSVP system",
-			"GUI for guest placement planning",
-			"user assigned checklist system",
-			"logs"
-		]
-	},
-	{
-		"active": false,
-		"title": "church-manager",
-		"features": [
-			"authentication",
-			"administrator CRUD",
-			"front/backend separation",
-			"calendar & financing features",
-			"API endpoints (+tests)",
-			"logs"
-		]
-	},
-	{
-		"active": false,
-		"title": "roleplaying-game",
-		"features": [
-			"authentication (also social media)",
-			"administrator CRUD",
-			"RPG/CMG/CCG features",
-			"player vs player mode",
-			"user ranking tables",
-			"logs"
-		]
-	},
-	{
-		"active": false,
-		"title": "stickers-social-media",
-		"features": [
-			"authentication (also social media)",
-			"administrator CRUD",
-			"front/backend separation",
-			"maps integration",
-			"befriending users",
-			"friend activities feed"
-		]
-	},
-	{
-		"active": false,
-		"title": "karaoke-app",
-		"features": [
-			"authentication (also social media)",
-			"administrator CRUD",
-			"video service integration",
-			"karaoke module",
-			"available song list explorer",
-			"traffic analytics"
-		]
-	},
-	{
-		"active": false,
-		"title": "kibol-alert",
-		"features": [
-			"authentication (also social media)",
-			"administrator CRUD",
-			"front/backend separation",
-			"geolocation services",
-			"club relations managment",
-			"logs"
-		]
-	},
-	{
-		"active": false,
-		"title": "family-hub",
-		"features": [
-			"family tree maker",
-			"restricted authentication",
-			"front/backend separation",
-			"multitenancy",
-			"news/event feed",
-			"shared photo galleries"
-		]
-	},
-	{
-		"active": true,
-		"title": "lessons-plan-app",
-		"features": [
-			"Google Calendar API",
-			"multiple systems integration",
-			"front/backend separation",
-			"ads management",
-			"logs",
-			"traffic analytics"
-		]
-	}
+    {
+        "active": false,
+        "title": "pizza-order-generator",
+        "features": [
+            "authentication (also social media)",
+            "administrator CRUD",
+            "multitenancy",
+            "drag&drop frontend",
+            "real-time actions",
+            "traffic analytics & logs"
+        ]
+    },
+    {
+        "active": false,
+        "title": "familiada-online",
+        "features": [
+            "authentication (also social media)",
+            "administrator CRUD",
+            "front/backend separation",
+            "real-time multi-user game",
+            "games archive",
+            "traffic analytics"
+        ]
+    },
+    {
+        "active": false,
+        "title": "ebook-self-publisher",
+        "features": [
+            "authentication",
+            "administrator CRUD",
+            "payment plans",
+            "advanced WYSIWYG editor",
+            "file conversion",
+            "logs"
+        ]
+    },
+    {
+        "active": false,
+        "title": "wedding-planner",
+        "features": [
+            "authentication (also social media)",
+            "administrator CRUD",
+            "invitations and RSVP system",
+            "GUI for guest placement planning",
+            "user assigned checklist system",
+            "logs"
+        ]
+    },
+    {
+        "active": false,
+        "title": "church-manager",
+        "features": [
+            "authentication",
+            "administrator CRUD",
+            "front/backend separation",
+            "calendar & financing features",
+            "API endpoints (+tests)",
+            "logs"
+        ]
+    },
+    {
+        "active": false,
+        "title": "roleplaying-game",
+        "features": [
+            "authentication (also social media)",
+            "administrator CRUD",
+            "RPG/CMG/CCG features",
+            "player vs player mode",
+            "user ranking tables",
+            "logs"
+        ]
+    },
+    {
+        "active": false,
+        "title": "stickers-social-media",
+        "features": [
+            "authentication (also social media)",
+            "administrator CRUD",
+            "front/backend separation",
+            "maps integration",
+            "befriending users",
+            "friend activities feed"
+        ]
+    },
+    {
+        "active": false,
+        "title": "karaoke-app",
+        "features": [
+            "authentication (also social media)",
+            "administrator CRUD",
+            "video service integration",
+            "karaoke module",
+            "available song list explorer",
+            "traffic analytics"
+        ]
+    },
+    {
+        "active": false,
+        "title": "kibol-alert",
+        "features": [
+            "authentication (also social media)",
+            "administrator CRUD",
+            "front/backend separation",
+            "geolocation services",
+            "club relations managment",
+            "logs"
+        ]
+    },
+    {
+        "active": false,
+        "title": "family-hub",
+        "features": [
+            "family tree maker",
+            "restricted authentication",
+            "front/backend separation",
+            "multitenancy",
+            "news/event feed",
+            "shared photo galleries"
+        ]
+    },
+    {
+        "active": true,
+        "title": "lessons-plan-app",
+        "features": [
+            "Google Calendar API",
+            "multiple systems integration",
+            "front/backend separation",
+            "ads management",
+            "logs",
+            "traffic analytics"
+        ]
+    },
+    {
+        "active": true,
+        "title": "prison-manager",
+        "features": [
+            "multitenancy",
+            "restricted authentication",
+            "cell & prisoners management",
+            "exposing restricted API",
+            "notifications",
+            "searchable logs"
+        ]
+    },
+    {
+        "active": true,
+        "title": "garage-expenses",
+        "features": [
+            "authentication (also social media)",
+            "expenses management",
+            "charts and figures and summaries",
+            "shared vehicles",
+            "front/backend separation",
+            "global & local logs"
+        ]
+    },
+    {
+        "active": true,
+        "title": "city-game",
+        "features": [
+            "authentication (also social media)",
+            "photo upload",
+            "maps integration",
+            "local rankings",
+            "game module",
+            "statistics and summaries"
+        ]
+    },
+    {
+        "active": true,
+        "title": "charades",
+        "features": [
+            "global & local CRUD",
+            "authentication (also social media)",
+            "front/backend separation",
+            "real-time actions",
+            "statistics + summaries",
+            "logs"
+        ]
+    },
+    {
+        "active": true,
+        "title": "where-have-i-been",
+        "features": [
+            "authentication (also social media)",
+            "map with visited places",
+            "user statistics + public profiles",
+            "2+ integrations",
+            "befriending users",
+            "front/backend separation"
+        ]
+    },
+    {
+        "active": true,
+        "title": "insurance-calculator",
+        "features": [
+            "front/backend separation",
+            "complex form validation",
+            "domain business logic",
+            "exposing public API",
+            "administrator CRUD",
+            "searchable logs"
+        ]
+    },
+    {
+        "active": true,
+        "title": "homebrewing-storage",
+        "features": [
+            "authentication (also social media)",
+            "storing diffrent types of products",
+            "notifications",
+            "handy interface",
+            "front/backend separation",
+            "logs"
+        ]
+    }
 ];
